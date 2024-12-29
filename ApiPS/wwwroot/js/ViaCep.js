e3d5f1a0006c91715b35e6321f0dc0f3b1175d82
Eduardo Kauê <eduardokaue277@gmail.com>
2024-12-29 00:03:12 -0300

    Feat: Implementação do VIACEP

---
+++ ApiPS/wwwroot/js/ViaCep.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const cep = document.getElementById('cep');
+    const logradouro = document.getElementById('logradouro');
+    const cidade = document.getElementById('cidade');
+    const estado = document.getElementById('estado');
+
+    cep.addEventListener('focusout', async () => {
+        const onlyNumbers = /^[0-9]{8}$/;
+
+        // Validação do CEP
+        if (!onlyNumbers.test(cep.value)) {
+            alert('CEP INVÁLIDO');
+            return;
+        }
+
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+
+            if (!response.ok) {
+                throw new Error('Erro ao buscar informações do CEP');
+            }
+
+            const responseCep = await response.json();
+
+            if (responseCep.erro) {
+                throw new Error('CEP não encontrado');
+            }
+
+            // Preenchendo os campos
+            logradouro.value = responseCep.logradouro || '';
+            cidade.value = responseCep.localidade || '';
+            estado.value = responseCep.uf || '';
+        } catch (error) {
+            console.error(error);
+            alert(error.message);
+        }
+    });
+});
